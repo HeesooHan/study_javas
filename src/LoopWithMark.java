5d833db75259ec442feae1ecff60bd362bbbae16
HeesooHan <hhs950824@gmail.com>
2023-05-24 15:32:12 +0900

    2023-05-24

---
+++ src/LoopWithMark.java
@@ -0,0 +1,9 @@
+public class LoopWithMark {
+    public static void main(String[] args) {
+        for (int line = 1 ; line < 5; line = line + 1) {
+            System.out.println("for " + line);
+        }
+        System.out.println("end");
+        //return 0 ;
+    }
+}
