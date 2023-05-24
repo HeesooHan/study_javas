703fb06b8a7669329d4e9ac9488f758ddf91ae59
HeesooHan <hhs950824@gmail.com>
2023-05-24 11:05:05 +0900

    2023-05-24

---
+++ src/LoopWithCounter.java
@@ -0,0 +1,12 @@
+public class LoopWithCounter {
+    public static void main(String[] args) {
+        int star = 1 ;
+        int bird = 5 ;
+        while (star <= bird) {
+            System.out.println(star);
+            star = star + 1 ;
+        }
+        System.out.println();
+        // return 0 ;
+    }
+}
